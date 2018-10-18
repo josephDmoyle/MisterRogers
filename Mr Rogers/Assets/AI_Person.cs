6763889be80393bcd2c229fab7aeba81713f83cd
josephDmoyle <u0929730@utah.edu>
2018-10-18 09:20:14 -0600

    Initial Commit

---
+++ Mr Rogers/Assets/AI_Person.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Person : MonoBehaviour {
+    public bool grasped = false;
+    [SerializeField] float speed;
+    Rigidbody body;
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+        Invoke("Decide", 1);
+    }
+    void Start () {
+		
+	}
+	
+	void FixedUpdate () {
+        
+    }
+
+    void Decide()
+    {
+        Debug.Log("Decide");
+        Invoke("Decide", 1);
+        body.velocity = Vector3.zero;
+        body.velocity = speed * new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f));
+    }
+}
